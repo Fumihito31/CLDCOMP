2a6d7b52b0fd50a3bb389f5eb22a745fb9615010
Fumihito31 <148638605+Fumihito31@users.noreply.github.com>
2024-06-17 15:48:39 +0800

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -2,5 +2,5 @@
 	define('DB_HOST', 'marimart-db.mysql.database.azure.com');
 	define('DB_USER', 'admin_user');
 	define('DB_PASS', 'password_123');
-	define('DB_NAME', 'minimartdb');
+	define('DB_NAME', 'marimart');
 ?>
